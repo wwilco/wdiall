55fcbd9f41fa9eadabacebb1c8d7bac3d4b810f6
will wilco <wgmwilcox@gmail.com>
2015-01-23 10:23:04 -0500

    Update insta.rb

--- w08/d01/day2_1/insta.rb
+++ w08/d01/day2_1/insta.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-  url = "https://api.instagram.com/v1/tags/dogs/media/recent?client_id=039e1f6aa31249328847c05d11da2bd0"
+  url = "https://api.instagram.com/v1/tags/dogs/media/recent?client_id="
   response = HTTParty.get(url)
   finder = response["data"][0]["images"]["standard_resolution"]["url"]
   finderx = {
